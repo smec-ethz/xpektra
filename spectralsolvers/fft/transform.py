fbe55acbd2ee9fdf810451a950608be12cd18fdc
Mohit Pundir <mpundir@ethz.ch>
2025-04-03 09:51:14 +0200

    adding forrward and inverse fft transform functions

---
+++ spectralsolvers/fft/transform.py
@@ -0,0 +1,36 @@
+from functools import partial
+import jax
+
+jax.config.update("jax_compilation_cache_dir", "/cluster/scratch/mpundir/jax-cache")
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+
+import jax.numpy as jnp  # type: ignore
+
+jax.config.update("jax_enable_x64", True)  # use double-precision
+jax.config.update("jax_platforms", "cpu")
+
+
+def _fft(x, N, ndim):
+    ndim = len(x.shape)
+    return jnp.fft.fftshift(
+        jnp.fft.fftn(
+            jnp.fft.ifftshift(x),
+            [
+                N,
+            ]
+            * ndim,
+        )
+    )
+
+
+def _ifft(x, N, ndim):
+    ndim = len(x.shape)
+    return jnp.fft.fftshift(
+        jnp.fft.ifftn(
+            jnp.fft.ifftshift(x),
+            [
+                N,
+            ]
+            * ndim,
+        )
+    )
